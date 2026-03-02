b1516035c98c3ead684a59d76037ea5990f5e70c
jinhae8971 <45819341+jinhae8971@users.noreply.github.com>
2026-03-02 12:47:36 +0900

    feat: 5인 에이전트 GlobalNewsAgent + 24h 뉴스 수집 통합

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,11 +1,13 @@
-from .tech_momentum_agent    import TechMomentumAgent
-from .macro_fed_agent        import MacroFedAgent
-from .sector_rotation_agent  import SectorRotationAgent
+from .tech_momentum_agent     import TechMomentumAgent
+from .macro_fed_agent         import MacroFedAgent
+from .sector_rotation_agent   import SectorRotationAgent
 from .value_fundamental_agent import ValueFundamentalAgent
+from .news_agent              import GlobalNewsAgent
 
 __all__ = [
     "TechMomentumAgent",
     "MacroFedAgent",
     "SectorRotationAgent",
     "ValueFundamentalAgent",
+    "GlobalNewsAgent",
 ]
